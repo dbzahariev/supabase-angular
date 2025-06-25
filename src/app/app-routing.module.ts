962e844fd5405eed9f9d41b66640f08dcd2c550a
Dimitar Zahariev <dbzahariev@gmail.com>
2025-06-25 09:48:48 +0300

    Update allowed CommonJS dependencies and adjust budget limits; remove unused import in routing module and clean up header styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
-import { HeaderComponent } from './header/header.component';
 import { AddPrediction } from './add-prediction/add-prediction';
 
 const routes: Routes = [
