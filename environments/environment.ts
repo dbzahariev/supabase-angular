5a78f5cbab10ab2e8a558c5436b1b833ef8350ad
Dimitar Zahariev <dbzahariev@gmail.com>
2026-01-31 19:48:06 +0200

    Fix environment configuration; update Supabase URL and key for correct API access

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://gebhwxodstlloeldbpdo.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlYmh3eG9kc3RsbG9lbGRicGRvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxOTA0NDIsImV4cCI6MjA2NDc2NjQ0Mn0.BnJ_97WDtd0zypRjLv6k8M4thmK3CEJCMXYpUKw_0-E',
+  supabaseUrl: 'https://gvletemnvogvblqlmzob.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd2bGV0ZW1udm9ndmJscWxtem9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk3NzY3NTAsImV4cCI6MjA4NTM1Mjc1MH0.NGp_l3whoft7Axia-DhI_v3CU_eOhXLbx2RKeRyQcSw',
 }
